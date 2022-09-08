27cff142517635904fc1eb069b85a2c735f75e50
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-09-08 18:43:33 +0100

    first commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,19 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\RUDEV\\Documents\\Meus App Flutter\\app_aprende_ingles" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\RUDEV\\Documents\\Meus App Flutter\\app_aprende_ingles"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\RUDEV\\Documents\\Meus App Flutter\\app_aprende_ingles\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\RUDEV\\Documents\\Meus App Flutter\\app_aprende_ingles"
+  "FLUTTER_TARGET=C:\\Users\\RUDEV\\Documents\\Meus App Flutter\\app_aprende_ingles\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\RUDEV\\Documents\\Meus App Flutter\\app_aprende_ingles\\.dart_tool\\package_config.json"
+)
